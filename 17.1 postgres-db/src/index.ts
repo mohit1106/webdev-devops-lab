8cda2faa862622dc1cd6720cbb70a98341545fcd
Mohit Verma <mohit51341862@gmail.com>
2025-10-31 03:17:21 +0530

    postgres

--- 17.1 postgres-db/src/index.ts
+++ 17.1 postgres-db/src/index.ts
@@ -1,15 +1,55 @@
+// connecting client to postgres database using pg to neon.tech
 import dotenv from "dotenv";
 dotenv.config();
 
 import {Client} from "pg";
-const pgClient = new Client({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    port: Number(process.env.PG_PORT),
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-})
+const pgClient = new Client(process.env.PG_CONNECTION_STRING)
 
 async function main() {
     await pgClient.connect();
+    console.log("✅ Connected to PostgreSQL");
+
+    const response = await pgClient.query("UPDATE users SET username='mohit10' WHERE id=2;");
+    console.log("📦 Query Result:", response.rows);
+
 }
+
+main();   
+
+
+
+
+
+
+
+
+
+// seeing how sql injection works in application's databases
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    try {
+        // it'll be vulnerable to sql injection
+        // const insertQuery = `INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`;
+        // console.log(insertQuery);
+        // const response = await pgClient.query(insertQuery);
+
+
+        // to prevent sql injection do this
+        const insertQuery2 = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`
+        const response = await pgClient.query(insertQuery2, [username, email, password]);
+
+        res.json({
+            message: "You have signed up"
+        })
+    }
+    catch(e) {
+        // Error handling code should go here
+    }
+});
