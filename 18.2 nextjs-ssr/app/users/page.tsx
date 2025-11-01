74ba9387a620ff05dbe71579af935fb2f5dd5d7f
Mohit Verma <mohit51341862@gmail.com>
2025-11-02 03:25:23 +0530

    ssr

---
+++ 18.2 nextjs-ssr/app/users/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+// in app dir, this page.tsx is inside users, so we can just directly open localhost:3000/users 
+export default function Home() {
+  return (
+    <div>
+      hi users page 
+    </div>
+  );
+}
