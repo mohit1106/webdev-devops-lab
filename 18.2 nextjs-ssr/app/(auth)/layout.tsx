9cf3375589137f0925d93b154fedc78646aea403
Mohit Verma <mohit51341862@gmail.com>
2025-11-04 05:12:32 +0530

    routegroups

--- 18.2 nextjs-ssr/app/(auth)/layout.tsx
+++ 18.2 nextjs-ssr/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
+import { ReactNode } from "react";
 
-export default function AuthLayout({children}) {
+export default function AuthLayout({children}:{children: ReactNode}) {
     return <div>
         <Navbar />
         {children}
@@ -8,3 +9,4 @@ export default function AuthLayout({children}) {
 }
 // auth is in () because in url we can then directly go to localhost:3000/signin
 // instead of localhost:3000/auth/signin
+// this allows us to apply layouts to all the pages that comes after it i.e only all child pages without affecting the url or any other pages.
