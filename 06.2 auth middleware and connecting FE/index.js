70498255014fd55245a831b7feab8535c0452a9e
Mohit Verma <mohit51341862@gmail.com>
2025-11-14 07:56:24 +0530

    aws ec2 CI CD

--- 06.2 auth middleware and connecting FE/index.js
+++ 06.2 auth middleware and connecting FE/index.js
@@ -15,6 +15,7 @@ app.get("/", function (req, res) {
 });
 
 
+
 app.post("/signup", function (req, res){
     const username = req.body.username;
     const password = req.body.password;
--- 09.1 react basic/App.jsx
+++ 09.1 react basic/App.jsx
@@ -22,6 +22,7 @@ export default function App() {
     });
     setTodos(newArray);
   }
+  
 
   return (
     <div>
